315a54a3633b359ad59217950d0bf7ae12105885
Yuvrajbh <yuvrajbhalla05@gmail.com>
2024-08-23 01:30:52 +0530

    small change

--- crowdup/src/main.tsx
+++ crowdup/src/main.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { CrowdFundingProvider } from "../../blockchain/context/CrowdFunding.jsx";
+// import { CrowdFundingProvider } from "../../blockchain/context/CrowdFunding.jsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <CrowdFundingProvider>
+    {/* <CrowdFundingProvider>/ */}
       <App />
-    </CrowdFundingProvider>
+    {/* </CrowdFundingProvider> */}
   </React.StrictMode>
 );
